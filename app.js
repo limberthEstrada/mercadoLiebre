2fd47a0e259311a559b6cdfc35d41bee164ee7f3
limberthEstrada <lvasquezestrada@frba.utn.edu.ar>
2021-11-23 11:11:03 -0300

    agrego cambio de nombre

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const app = express()
 const port = 3000
 app.use(express.static(rutaPath));
 
+app.listen (process.env.PORT ||3000, ()=>{
+    console.log('Servidor funcionando bien');
+});
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./views/home.html'))
